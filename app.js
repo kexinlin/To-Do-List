44af9d0ca235c91fb800b4e7999c30cbcae3a03d
kexinlin <kexin.lin@mail.utoronto.ca>
2021-01-09 21:52:35 +0800

    implement todo-list with date as title

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.get("/", (req, res) => {
+  let today = new Date();
+  let options = { weekday: "long", day: "numeric", month: "long" };
+  let day = today.toLocaleDateString("en-us", options);
+  res.render("list", { kindOfDay: day, listItems: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server running on http://localhost:3000/...");
+});
