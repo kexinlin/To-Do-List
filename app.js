56d77c6cba8cd7c26be9c558f7f14bd51d86fb0e
kexinlin <kexin.lin@mail.utoronto.ca>
2021-01-12 18:33:12 +0800

    update port for Heroku, update gitignore and add procfile

--- app.js
+++ app.js
@@ -108,6 +108,6 @@ app.get("/:customListName", (req, res) => {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server running on http://localhost:3000/...");
 });
